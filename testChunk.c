c8d7c4794841be93d60c23288467a685e62334cf
Damien Churchill <damien.churchill@ukplc.net>
2011-06-23 12:28:12 +0100

    test creating a chunk object

--- testChunk.c
+++ testChunk.c
@@ -1,21 +1,40 @@
+#include <assert.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "chunk.h"
 
-void test_new_chunk() {
-    unsigned char buffer[4], hash[20];
-    Chunk chunk;
-
-    memcpy(buffer, "test", 4);
-    memcpy(hash, "a94a8fe5ccb19ba61c4c0873d391e987982fbbd3", 20);
-
-    chunk = bs_new_chunk(1, buffer, hash, 4, 20);
-}
+void test_chunk();
 
 int main(void)
 {
-    printf("Running test_new_chunk()\n");
-    test_new_chunk();
+    printf("Running test_chunk()\n");
+    test_chunk();
 
     return 0;
+}
+
+void test_chunk() {
+    unsigned char *buffer, *hash;
+    Chunk chunk;
+
+    buffer = malloc(4);
+    hash = malloc(20);
+
+    memcpy(buffer, "test", 4);
+    memcpy(hash, "thistesthashforatest", 20);
+
+    chunk = bs_new_chunk(1, buffer, hash, 4, 20);
+
+    assert(memcmp(chunk->data, buffer, 4));
+    assert(memcmp(chunk->hash, hash, 20));
+    assert(chunk->number == 1);
+    assert(chunk->chunk_size == 4);
+    assert(chunk->hash_length == 20);
+
+    free(buffer);
+    free(hash);
+
+    assert(memcmp(chunk->data, "test", 4));
+    assert(memcmp(chunk->hash, "thistesthashforatest", 20));
 }
